0b0f8de98cf90aef196686adde9f53c2e3ab0c76
Hia <120297866+hiapb@users.noreply.github.com>
2026-03-19 14:45:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -375,6 +375,13 @@ install_api_shield() {
     exit 0
 }
 
+install_nic-master(){
+    clear
+    echo -e "${GREEN}正在进入 nic-master...${RESET}"
+    bash <(curl -fsSL https://raw.githubusercontent.com/inimemail/nic-master/main/install.sh)
+    exit 0
+}
+
 install_haproxy() {
     clear
     echo -e "${GREEN}正在安装 HAProxy TCP转发管理脚本...${RESET}"
@@ -776,6 +783,7 @@ show_menu() {
     echo "42) Sub2API 一键管理"
     echo "43) CLIProxyAPI 一键管理"
     echo "44) 零信任矩阵网关系统"
+    echo "45) nic-master"
     echo "u) 更新 HIA 管理脚本"
     echo "q) 卸载 HIA 管理脚本"
     echo "0) 退出"
@@ -826,6 +834,7 @@ show_menu() {
         42) install_qm_s2a ;;
         43) install_qm_cli ;;
         44) install_api_shield ;;
+        45) install_nic-master;;
         u)  update_hia ;;
         q)  uninstall_hia ;;
         0)  exit 0 ;;
