cd25a4f05467ebad56860f9e636831ad12e7c61a
Hia <120297866+hiapb@users.noreply.github.com>
2026-04-25 21:38:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -880,7 +880,7 @@ show_menu() {
     echo "44) 零信任矩阵网关系统"
     echo "45) nic-master"
     echo "46) Forgejo 一键管理"
-    echo "46) hia-X 一键管理"
+    echo "47) hia-X 一键管理"
     echo "u) 更新 HIA 管理脚本"
     echo "q) 卸载 HIA 管理脚本"
     echo "0) 退出"
